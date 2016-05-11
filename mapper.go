20a22730d555b667361e48a06ac416bafbe7f1ea
Mariano Cano <mariano@betable.com>
2016-05-11 16:19:31 -0700

    Add ecql mapper.

---
+++ mapper.go
@@ -0,0 +1,153 @@
+package ecql
+
+import (
+	"reflect"
+	"strings"
+)
+
+var (
+	// TAG_COLUMNS is the tag used in the structs to set the column name for a field.
+	// If a name is not set, the name would be the lowercase version of the field.
+	// If you want to skip a field you can use `cql:"-"`
+	TAG_COLUMN = "cql"
+
+	// TAG_TABLE is the tag used in the structs to define the table for a type.
+	// If the table is not set it defaults to the type name in lowercase.
+	TAG_TABLE = "cqltable"
+
+	// TAG_KEY defines the primary key for the table.
+	// If the table uses a composite key you just need to define multiple columns
+	// separated by a comma: `cqlkey:"id"` or `cqlkey:"partkey,id"`
+	TAG_KEY = "cqlkey"
+)
+
+var registry = make(map[reflect.Type]Table)
+
+// Table contains the information of a table in cassandra.
+type Table struct {
+	Name      string
+	KeyColumn string
+	Columns   []Column
+}
+
+// Column contains the information of a column in a table required
+// to create a map for it.
+type Column struct {
+	Name     string
+	Position int
+}
+
+// Delete registry cleans the registry.
+// This would be mainly used in unit testing.
+func DeleteRegistry() {
+	registry = make(map[reflect.Type]Table)
+}
+
+// Register adds the passed struct to the registry to be able to use gocql
+// MapScan methods with struct types.
+//
+// It maps the columns using the struct tag 'cql' or the lowercase of the
+// field name. You can skip the mapping of one field using the tag `cql:"-"`
+func Register(i interface{}) {
+	register(i)
+}
+
+// Map creates a new map[string]interface{} where each member in the map
+// is a reference to a field in the struct. This allows to assign values
+// to a struct using gocql MapScan.
+//
+// Given a gocql session, the following code will populate the struct 't'
+// with the values in the datastore.
+// 	var t MyStruct
+// 	query := session.Query("select * from mytable where id = ?", "my-id")
+// 	m := cql.Map(&t)
+// 	err := query.MapScan(m)
+func Map(i interface{}) map[string]interface{} {
+	columns, _ := MapTable(i)
+	return columns
+}
+
+// MapTable creates a new map[string]interface{} where each member in the map
+// is a reference to a field in the struct. This allows to assign values
+// to a struct using gocql MapScan. MapTable also returns the Table with the
+// information about the type.
+//
+// Given a gocql session, the following code will populate the struct 't'
+// with the values in the datastore.
+// 	var t MyStruct
+// 	query := session.Query("select * from mytable where id = ?", "my-id")
+// 	m, _ := cql.MapTable(&t)
+// 	err := query.MapScan(m)
+func MapTable(i interface{}) (map[string]interface{}, Table) {
+	v := structOf(i)
+	t := v.Type()
+
+	// Get the table or register on the fly if necessary
+	table, ok := registry[t]
+	if !ok {
+		// table = register(i)
+		table = register(i)
+	}
+
+	columns := make(map[string]interface{})
+	for _, col := range table.Columns {
+		field := v.Field(col.Position)
+		if field.CanAddr() {
+			columns[col.Name] = field.Addr().Interface()
+		} else {
+			columns[col.Name] = field.Interface()
+		}
+	}
+	return columns, table
+}
+
+func structOf(i interface{}) reflect.Value {
+	v := reflect.ValueOf(i)
+	switch v.Kind() {
+	case reflect.Struct:
+		return v
+	case reflect.Ptr, reflect.Interface:
+		elem := v.Elem()
+		if elem.Kind() == reflect.Struct {
+			return elem
+		}
+	}
+
+	panic("register type is not struct")
+}
+
+func register(i interface{}) Table {
+	v := structOf(i)
+	t := v.Type()
+
+	// Table name defaults to the type name.
+	var table Table
+	table.Name = t.Name()
+
+	for i, n := 0, t.NumField(); i < n; i++ {
+		field := t.Field(i)
+		// Get table if available
+		name := field.Tag.Get(TAG_TABLE)
+		if name != "" {
+			table.Name = name
+		}
+
+		// Get the key columns
+		// FIXME: add a default behavior, ie: first field
+		name = field.Tag.Get(TAG_KEY)
+		if name != "" {
+			table.KeyColumn = name
+		}
+
+		// Get columns or field name
+		name = field.Tag.Get(TAG_COLUMN)
+		if name == "" {
+			name = strings.ToLower(field.Name)
+		}
+		if name != "-" {
+			table.Columns = append(table.Columns, Column{name, i})
+		}
+	}
+	registry[t] = table
+	return table
+}
---
+++ mapper_test.go
@@ -0,0 +1,120 @@
+package ecql
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type testStruct struct {
+	F1 string `cql:"f1" cqltable:"mytable" cqlkey:"f1"`
+	F2 int    `cql:"f22"`
+	F3 map[string]string
+	F4 *string `cql:""`
+	F5 string  `cql:"-"`
+}
+
+var testStructNames = []string{"f1", "f22", "f3", "f4"}
+
+func TestRegister(t *testing.T) {
+	var tests = []struct {
+		I interface{}
+	}{
+		{testStruct{}},
+		{&testStruct{}},
+	}
+
+	typ := reflect.TypeOf(testStruct{})
+	for _, tc := range tests {
+		DeleteRegistry()
+		Register(tc.I)
+		table, ok := registry[typ]
+		assert.True(t, ok)
+		assert.Equal(t, "mytable", table.Name)
+		assert.Equal(t, "f1", table.KeyColumn)
+		assert.Len(t, table.Columns, 4)
+		for i := range testStructNames {
+			assert.Equal(t, testStructNames[i], table.Columns[i].Name)
+			assert.Equal(t, i, table.Columns[i].Position)
+		}
+	}
+}
+
+func TestMap(t *testing.T) {
+	DeleteRegistry()
+
+	s1 := "string-1"
+	s2 := "string-2"
+	ts := testStruct{
+		F1: "foo",
+		F2: 123,
+		F3: map[string]string{"foo": "bar"},
+		F4: &s1,
+		F5: "zar",
+	}
+	exp := testStruct{
+		F1: "foobar",
+		F2: 321,
+		F3: map[string]string{"foobar": "zar"},
+		F4: &s2,
+		F5: "zar",
+	}
+
+	var tests = []struct {
+		name string
+		ok   bool
+		v    interface{}
+		vv   interface{}
+	}{
+		{"f1", true, ts.F1, exp.F1},
+		{"f22", true, ts.F2, exp.F2},
+		{"f3", true, ts.F3, exp.F3},
+		{"f4", true, ts.F4, exp.F4},
+		{"f5", false, nil, exp.F5},
+	}
+
+	// With registry and passing as a value
+	Register(testStruct{})
+	m := Map(ts)
+	for _, tc := range tests {
+		v, ok := m[tc.name]
+		assert.Equal(t, tc.ok, ok)
+		assert.Equal(t, tc.v, v)
+		m[tc.name] = tc.vv
+	}
+	assert.NotEqual(t, exp, ts)
+
+	// Without registry and passing as a reference
+	DeleteRegistry()
+	m = Map(&ts)
+	for _, tc := range tests {
+		v, ok := m[tc.name]
+		vv := reflect.ValueOf(v)
+		assert.Equal(t, tc.ok, ok)
+		if tc.name != "f5" {
+			assert.Equal(t, tc.v, vv.Elem().Interface())
+			vv.Elem().Set(reflect.ValueOf(tc.vv))
+		}
+	}
+	assert.Equal(t, exp, ts)
+}
+
+func TestStructOfPanic1(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("Panic not executed")
+		}
+	}()
+	Register("string")
+}
+
+func TestStructOfPanic2(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("Panic not executed")
+		}
+	}()
+	s := "string"
+	Register(&s)
+}
